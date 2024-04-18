e1eade10f05d3cc77ebe80207c197aac45af7b96
bswck <bswck.dev@gmail.com>
2024-04-18 03:46:39 +0200

    Fix type-checking typo

--- lazy_importing/compat.py
+++ lazy_importing/compat.py
@@ -20,12 +20,13 @@ def accesses_lazy_objects(f: Callable[P, R]) -> Callable[P, R]:
     """
     Decorate a function for CPython 3.8 and 3.9 compatibility with lazy importing.
 
-    Its sole purpose is to create an additional external frame before a function
-    is called. That ensures the [lazy object loader](lazy_importing.cm.LazyObjectLoader)
-    is requested a missing identifier after the function is.
+    The sole purpose of this function is to create an additional external frame
+    before the decorated function `f` is called. This ensures that
+    the [lazy object loader](lazy_importing.cm.LazyObjectLoader)
+    is requested a missing identifier during the function being called.
     """
 
-    def wrapper(*args: P.args, **kwargs: R.kwargs) -> R:
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
         return f(*args, **kwargs)
 
     return wrapper
