0127564e4e76f21e5feed563e326d2c945fa593c
bswck <bswck.dev@gmail.com>
2024-04-22 14:59:42 +0200

    Remove tests for rewrite

--- tests/conftest.py
+++
@@ -1,6 +0,0 @@
-import sys
-from os.path import dirname
-
-sys.path.append(f"{dirname(__file__)}/lazy_modules/")
-
-collect_ignore = ["lazy_modules/"]
--- tests/lazy_modules/lazy_namespace/inner_lazy_module.py
+++
@@ -1,5 +0,0 @@
-from tests.state import assert_lazy_loading
-
-assert_lazy_loading(__name__)
-
-lazy_object = __file__
