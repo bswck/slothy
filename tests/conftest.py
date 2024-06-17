2848d6403acc365bda07d59b1e5cefb4905524b1
bswck <bartoszpiotrslawecki@gmail.com>
2024-06-17 04:57:36 +0200

    Fix test collection

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
-__import__("sys").path.append("tests/import_target_layouts/")
+__import__("sys").path.append("tests/tested_layouts/")
 
 collect_ignore = ["tests.py"]
 collect_ignore_glob = [
-    "import_target_layouts/*",
+    "tested_layouts/*",
 ]
